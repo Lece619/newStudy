c5a0fc883f25db056263a7b41772c697506d8fd6
L <lece619@gmail.com>
2022-05-05 19:42:05 +0900

    TimeOver

--- backjoon/stackQ/src/Num17298.java
+++ backjoon/stackQ/src/Num17298.java
@@ -8,53 +8,53 @@
 *
 * */
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Num17298 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] nums = new int[n];
-        String[] number=new String[n];
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        int n =Integer.parseInt(br.readLine());
+        StringTokenizer st=new StringTokenizer(br.readLine());
         int[] result=new int[n];
-        sc.nextLine();
-        number=sc.nextLine().split(" ");
-        for (int i = 0; i < nums.length; i++) {
-            nums[i]=Integer.parseInt(number[i]);
-            result[i]=-1;
+
+
+        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> stack2= new Stack<>();
+
+        for (int i = 0; i < n ; i++) {
+            stack.push(Integer.parseInt(st.nextToken()));
         }
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j < nums.length; j++) {
-                if(nums[i]<nums[j]) {
-                    result[i]=nums[j];
-                    break;
+        int idx=n-1;
+        while(!stack.isEmpty()){
+            int num = stack.pop();
+            if(stack2.isEmpty()){
+                result[idx] = -1;
+                idx--;
+            }else {
+                boolean flag = true;
+                while (stack2.peek() < num) {
+                    stack2.pop();
+                    if (stack2.isEmpty()) {
+                        result[idx] = -1;
+                        idx--;
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag){
+                    result[idx]=stack2.peek();
+                    idx--;
                 }
             }
+            stack2.push(num);
         }
         for (int i : result) {
             System.out.print(i+" ");
         }
-
-//        Stack<Integer> stack = new Stack<>();
-//        Stack<Integer> result= new Stack<>();
-//        int maxNum,currentNum,nextNum;
-//        for (int i = 0; i < n ; i++) {
-//            stack.push(sc.nextInt());
-//        }
-//        result.push(-1);
-//        maxNum=stack.pop();
-//        currentNum=maxNum;
-//        while(!stack.isEmpty()){
-//            nextNum=stack.pop();
-//            if(nextNum>=maxNum){
-//                result.push(-1);
-//                maxNum=nextNum;
-//                continue;
-//            }
-//
-            // 5 3 1 8 6 7 9
-
-        //}
     }
 }
