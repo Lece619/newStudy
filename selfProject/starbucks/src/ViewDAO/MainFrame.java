f67046e96e23a1486172a41b1a0e7df83d1240dd
L <lece619@gmail.com>
2022-05-11 21:39:46 +0900

    FrameTest

--- selfProject/starbucks/src/ViewDAO/MainFrame.java
+++ selfProject/starbucks/src/ViewDAO/MainFrame.java
@@ -1,69 +1,23 @@
 package ViewDAO;
 
 import javax.swing.*;
-import javax.swing.border.LineBorder;
 import java.awt.*;
 
 public class MainFrame extends JFrame {
-    Dimension monitorSize = Toolkit.getDefaultToolkit().getScreenSize();
-    int monitorX = monitorSize.width;
-    int monitorY = monitorSize.height;
-    Image background = Toolkit.getDefaultToolkit().getImage("D:\\git\\newStudy\\selfProject\\starbucks\\src\\Img\\backgroundImg.jpg");
-
-    public MainFrame() {
-
-        //배경이미지 설정
-        //background = Toolkit.getDefaultToolkit().getImage("./Img/backgroundImg.jpg");
-        background=background.getScaledInstance(monitorY/3*2,monitorY/4*3,Image.SCALE_SMOOTH);
-
-        //메인뷰 설정
-        setTitle("starbucks에 어서오세요");
-        setBounds(monitorX/4,50,monitorY/3*2,monitorY/4*3);
-        //Container mainPanel=this.getContentPane();
-        setLayout(new GridLayout(3,1));
-        //remove(getContentPane());
-        JLabel backLabel = new JLabel(new ImageIcon(background));
-        getContentPane().setLayout(new FlowLayout());
-        getContentPane().add(backLabel);
-        //add(backLabel);
-        JButton loginBtn = new JButton("로그인");
-        JButton joinBtn = new JButton("회원가입");
-
-        loginBtn.setPreferredSize(new Dimension(getWidth()/5,getWidth()/7));
-        joinBtn.setPreferredSize(new Dimension(getWidth()/5,getWidth()/7));
-        getContentPane().add(loginBtn);
-        getContentPane().add(joinBtn);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //Image image = new ImageIcon(MainFrame.class.getResource("Img/backgroundImg.jpg")).getImage();
+    public void startFrame(){
+        setBounds(100,100,100,100);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setBackground(Color.BLACK);
         setVisible(true);
-        /*mainPanel.setBackground(Color.RED);
-        BackPanel backgroundPanel = new BackPanel();
-        backgroundPanel.setVisible(true);
-        backgroundPanel.setBorder(new LineBorder(Color.BLACK,3));
-        mainPanel.add(backgroundPanel);*/
     }
-   /* //이너클래스 선언
-    class BackPanel extends JPanel{
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            g.drawImage(background,this.getX(),this.getY(),null);
-        }
 
-        @Override
-        public void paint(Graphics g) {
-            g.drawImage(background,this.getX(),this.getY(),null);
-        }
-    }*/
-
-    /*@Override
+    @Override
     public void paint(Graphics g) {
-        //super.paint(g);
-        System.out.println("in");
-        g.drawImage(background,0,0,null);
-        g.fillOval(0,0,30,30);
-    }*/
+        g.drawImage(image,0,0,null);
+    }
 
     public static void main(String[] args) {
-        new MainFrame();
+        new MainFrame().startFrame();
     }
 }
