8c900fa6667c8274af7f45f2df27f1145dd14478
L <lece619@gmail.com>
2022-05-11 23:50:00 +0900

    main

--- selfProject/starbucks/src/ViewDAO/MainFrame.java
+++ selfProject/starbucks/src/ViewDAO/MainFrame.java
@@ -7,14 +7,27 @@ public class MainFrame extends JFrame {
     //Image image = new ImageIcon(MainFrame.class.getResource("D:\\git\\newStudy\\selfProject\\starbucks\\src\\Img\\backgroundImg.jpg")).getImage();
     Image background = Toolkit.getDefaultToolkit().getImage("D:\\git\\newStudy\\selfProject\\starbucks\\src\\Img\\backgroundImg.jpg");
 
-    public void startFrame(){
-        setBounds(100,100,100,100);
+    public void startFrame() {
+        setBounds(300, 100, 700, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setBackground(Color.BLACK);
+        JLabel backLabel = new JLabel(){
+            @Override
+            protected void paintComponent(Graphics g) {
+                g.drawImage(background, 0, 0, null);
+                setOpaque(false);
+                super.paintComponent(g);
+            }
+        };
         JButton loginBtn = new JButton("로그인");
         JButton joinBtn = new JButton("회원가입");
+        loginBtn.setPreferredSize(new Dimension(100,60));
+        joinBtn.setPreferredSize(new Dimension(100,60));
+        setLayout(new FlowLayout());
+        loginBtn.setSize(100,60);
+        joinBtn.setSize(100,60);
         loginBtn.setVisible(true);
-
+        joinBtn.setVisible(true);
         add(loginBtn);
         add(joinBtn);
 
@@ -23,7 +36,7 @@ public class MainFrame extends JFrame {
 
     @Override
     public void paint(Graphics g) {
-        g.drawImage(background,0,0,null);
+        g.drawImage(background, 0, 0, null);
     }
 
     public static void main(String[] args) {
