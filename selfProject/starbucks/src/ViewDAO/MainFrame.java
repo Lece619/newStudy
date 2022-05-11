4924e2ffea38addb83cfb2be1fffb034a0834daf
L <lece619@gmail.com>
2022-05-11 23:38:35 +0900

    MainFrame

--- selfProject/starbucks/src/ViewDAO/MainFrame.java
+++ selfProject/starbucks/src/ViewDAO/MainFrame.java
@@ -4,17 +4,26 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MainFrame extends JFrame {
-    //Image image = new ImageIcon(MainFrame.class.getResource("Img/backgroundImg.jpg")).getImage();
+    //Image image = new ImageIcon(MainFrame.class.getResource("D:\\git\\newStudy\\selfProject\\starbucks\\src\\Img\\backgroundImg.jpg")).getImage();
+    Image background = Toolkit.getDefaultToolkit().getImage("D:\\git\\newStudy\\selfProject\\starbucks\\src\\Img\\backgroundImg.jpg");
+
     public void startFrame(){
         setBounds(100,100,100,100);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setBackground(Color.BLACK);
+        JButton loginBtn = new JButton("로그인");
+        JButton joinBtn = new JButton("회원가입");
+        loginBtn.setVisible(true);
+
+        add(loginBtn);
+        add(joinBtn);
+
         setVisible(true);
     }
 
     @Override
     public void paint(Graphics g) {
-        g.drawImage(image,0,0,null);
+        g.drawImage(background,0,0,null);
     }
 
     public static void main(String[] args) {
