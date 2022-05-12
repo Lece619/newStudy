b3827ba250421b3018e81517cb6229f1642de187
L <lece619@gmail.com>
2022-05-12 17:55:33 +0900

    re

---
+++ programmers/Level1/src/kakao2022/Report.java
@@ -0,0 +1,16 @@
+package kakao2022;
+//https://programmers.co.kr/learn/courses/30/lessons/92334
+public class Report {
+
+    public static void main(String[] args) {
+        String[] id_list ={"muzi", "frodo", "apeach", "neo" };
+        String[] report ={"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+        int k = 2;
+    }
+}
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
+        return answer;
+    }
+}
