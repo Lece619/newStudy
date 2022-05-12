055cedea50d673453dd8ca6105560904bf6faebd
L <lece619@gmail.com>
2022-05-12 21:54:23 +0900

    Create NewID2.java

---
+++ programmers/Level1/src/kakao2021/NewID2.java
@@ -0,0 +1,21 @@
+package kakao2021;
+
+public class NewID2 {
+    public String solution(String new_id){
+        String answer= "";
+
+        return "";
+    }
+
+    private static class KAKAOID{
+        private String s;
+        KAKAOID(String s){
+            this.s = s;
+        }
+
+        private KAKAOID replaceToLowerCase(){
+            s=s.toLowerCase();
+            return this;
+        }
+    }
+}
