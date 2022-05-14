c96ec1ec545bfce8beda69b9ab88bae0889884a3
L <lece619@gmail.com>
2022-05-14 20:10:14 +0900

    KeyPad

--- programmers/Level1/src/kakao2020/KeyPad.java
+++ programmers/Level1/src/kakao2020/KeyPad.java
@@ -10,9 +10,9 @@ import java.util.*;
 * */
 public class KeyPad {
     public static void main(String[] args) {
-        int[] numbers={1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
-        String hand="right";
-        new Solution().solution(numbers,hand);
+        int[] numbers={7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2};
+        String hand="left";
+        System.out.println(new Solution().solution(numbers,hand));
     }
     static class Solution {
         public String solution(int[] numbers, String hand) {
@@ -29,21 +29,47 @@ public class KeyPad {
             keyMap.put('*', new Integer[]{0, 3});
             keyMap.put('0', new Integer[]{1, 3});
             keyMap.put('#', new Integer[]{2, 3});
-
+            hand = hand.equals("left") ? "L" : "R";
             char left = '*';
             char right = '#';
             String answer = "";
 
             for (int i = 0; i < numbers.length; i++) {
-                char number = (char)numbers[i];
+                char number = Integer.toString(numbers[i]).charAt(0);
                 Integer[] currentNum=keyMap.get(number);
-                int lenghLeft = keyMap.get(left)[0]-currentNum[0]+
-                                    keyMap.get(left)[1] - currentNum[1];
-                int lenghRight = keyMap.get(right)[0] - currentNum[0]+
-                                    keyMap.get(left)[1] - currentNum[1];
-                if(lenghLeft<lenghRight){
-                    answer+=(int)number;
+                if(number!='2'&&number!='5'&&number!='8'&&number!='0'){
+                    if(number=='1'||number=='4'||number=='7'){
+                        left=number;
+                        answer+="L";
+
+                    }else{
+                        right=number;
+                        answer+="R";
+                    }
                 }
+                else {
+
+                    int lenghLeft = Math.abs(keyMap.get(left)[0] - currentNum[0]) +
+                            Math.abs(keyMap.get(left)[1] - currentNum[1]);
+                    int lenghRight = Math.abs(keyMap.get(right)[0] - currentNum[0]) +
+                            Math.abs(keyMap.get(right)[1] - currentNum[1]);
+                    if (lenghLeft < lenghRight) {
+                        answer += "L";
+                        left = number;
+                    } else if (lenghLeft == lenghRight) {
+                        answer += hand;
+                        if (hand.equals("L")) {
+                            left = number;
+                        } else {
+                            right = number;
+                        }
+                    } else {
+                        answer += "R";
+                        right = number;
+                    }
+                }
+                System.out.println(answer);
+                System.out.println(number+"누르기 "+"left : " + left + "  right : "+right);
             }
 
             return answer;
