70d2e693c9d93d12a45720639aba410fd67ffad6
L <lece619@gmail.com>
2022-05-18 17:09:21 +0900

    clear 10816
    카드놀이2

--- backjoon/mapSetQ/src/Num10816.java
+++ backjoon/mapSetQ/src/Num10816.java
@@ -3,6 +3,30 @@
 *  이 수가 적혀있는 숫자 카드를 상근이가 몇 개 가지고 있는지 구하는 프로그램을 작성하시오.
 * */
 
-public class Num10816 {
+import java.io.*;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
+public class Num10816 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        HashMap<Long,Integer> card = new HashMap<>();
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        while(st.hasMoreTokens()){
+            long key =Long.parseLong(st.nextToken());
+            card.put(key,card.getOrDefault(key,0)+1);
+        }
+        br.readLine();
+        st= new StringTokenizer(br.readLine());
+        while(st.hasMoreTokens()){
+            long key =Long.parseLong(st.nextToken());
+            bw.write(card.getOrDefault(key,0)+" ");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
 }
