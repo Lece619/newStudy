ec078c002fe1ae8243c1e24001eee95795791b11
L <lece619@gmail.com>
2022-05-24 18:51:04 +0900

    programmers1

--- programmers/Level1/src/normal/SortString.java
+++ programmers/Level1/src/normal/SortString.java
@@ -1,6 +1,7 @@
 package normal;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class SortString {
     public static void main(String[] args) {
@@ -8,6 +9,7 @@ public class SortString {
         char A = 'A';
         int b =a ;
         System.out.println(b+" "+(int)a+""+(int)A);
+        new SortString().new Solution().solution("Zbcdefg");
     }
 class Solution {
     public String solution(String s) {
@@ -15,9 +17,22 @@ class Solution {
         char[] chars= new char[s.length()];
         for (int i = 0; i < s.length(); i++) {
             chars[i]=s.charAt(i);
+            System.out.println(chars[i]);
         }
         Arrays.sort(chars);
+        //System.out.println(chars);
+        for (int i = chars.length-1; i >= 0; i--) {
+            answer+=chars[i];
+        }
+        System.out.println("answer  "+answer);
         return answer;
     }
 }
+/*
+*    public String reverseStr(String str){
+        return Stream.of(str.split(""))
+    .sorted(Comparator.reverseOrder())
+    .collect(Collectors.joining());
+    }
+* */
 }
