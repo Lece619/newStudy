2a01811bf6948611da1c4256529870f11851062e
L <lece619@gmail.com>
2022-05-27 15:31:26 +0900

    clear

--- programmers/level2/src/kakao2020/StringCompression2.java
+++ programmers/level2/src/kakao2020/StringCompression2.java
@@ -2,56 +2,48 @@ package kakao2020;
 
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Stack;
 
 public class StringCompression2 {
     public int solution(String s) {
 
-        if(s.length()==0) return 1;
-        //7 글자 짜리 3
-        //0 3 3 - 6 6~
-        int answer = 0;
         int len = s.length()/2;
-        int limit = s.length()-len-s.length()%len;
+        int min =s.length();
         for (int k = 1; k <= len; k++) {
 
             Queue<String> queue = new LinkedList<>();
+            String temp = "";
+            int cnt = 1;
+
             for (int i = 0; i < s.length(); i += k) {
-
-
-                System.out.println(i + "  " + limit);
-
                 if (i + k > s.length()) {
-                    System.out.println(s.substring(i));
                     queue.add(s.substring(i));
                 } else {
-                    System.out.println(s.substring(i, i + k));
                     queue.add(s.substring(i, i + k));
                 }
             }
-            String temp = "";
-            String now = queue.poll();
-            int cnt = 0;
-            while(!queue.isEmpty()){
+            int queueSize = queue.size();
+
+            for (int i = 0; i < queueSize; i++) {
+                String now = queue.poll();
                 if(now.equals(queue.peek())){
-                    queue.poll();
                     cnt++;
                 }else{
-                    if(cnt!=0){
-                        temp=cnt+now+temp;
+                    if(cnt!=1){
+                        temp=temp+cnt+now;
+                        cnt=1;
+                    }else{
+                        temp=temp+now;
                     }
-                    cnt=0;
-                    now = queue.poll();
                 }
-
             }
-            System.out.println("temp : "+temp);
+            min=Math.min(temp.length(),min);
         }
 
-        return answer;
+        return min;
     }
 
     public static void main(String[] args) {
-        new StringCompression2().solution("abcdefghi");
+
+        new StringCompression2().solution("aaaa");
     }
 }
