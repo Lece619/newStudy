3fe8d88bb35b8d24d86e7d54c157f0d448e2934d
L <lece619@gmail.com>
2022-05-28 14:48:44 +0900

    11659

--- backjoon/sumNum/src/Num11659.java
+++ backjoon/sumNum/src/Num11659.java
@@ -8,19 +8,16 @@ public class Num11659 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N];
+        int[] arr = new int[N+1];
         st=new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i]=Integer.parseInt(st.nextToken());
+        for (int i = 1; i < N+1; i++) {
+            arr[i]=arr[i-1]+Integer.parseInt(st.nextToken());
         }
         for (int i = 0; i < M; i++) {
             st=new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            int sum = 0;
-            for (int j = start-1; j <end; j++) {
-                sum+=arr[j];
-            }
+            int sum = arr[end]-arr[start-1];
             bw.write(sum+"\n");
         }
         bw.flush();
