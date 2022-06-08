8bcf3bcb2cc56e9c5dcc48aeb67035ec776b2d90
L <lece619@gmail.com>
2022-06-08 15:54:02 +0900

    joystick

--- programmers/level2/src/greedy/JoyStick.java
+++ programmers/level2/src/greedy/JoyStick.java
@@ -4,31 +4,64 @@
 package greedy;
 
 public class JoyStick {
+    static int[] move;
+    int nowIndex;
     public int solution(String name) {
         int answer = 0;
-        int count = name.length();
-        boolean[] check = new boolean[name.length()];
-        int[] move = new int[name.length()];
+        move = new int[name.length()];
         for (int i = 0; i < name.length(); i++) {
             int toA = name.charAt(i)-'A';
             int toB = 'Z'-name.charAt(i)+1;
             move[i] = Math.min(toA,toB);
         }
-        for (int i : move) {
-            System.out.print(" "+i);
+        answer += move[0];
+        move[0]=0;
+        nowIndex = 0;
+        int a = 0 ;
+        while(!((a = findZero()) == 0)){
+            answer += a;
         }
-
         return answer;
     }
-    //JAN
-    //
+    public int findZero(){
+        int down = nowIndex;
+        int up = nowIndex;
+        int count = 0;
+        boolean flag = false;
 
-    public int findNum(){
-        return 0;
+        for (int i = 1; i < move.length; i++) {
+            up++;
+            down--;
+            count++;
+            if(up== move.length){
+                up = 0;
+            }
+            if(down<0){
+                down = move.length-1;
+            }
+            if(move[up]!=0){
+                count += move[up];
+                move[up] = 0;
+                nowIndex = up;
+                flag = true;
+                break;
+            }
+            if(move[down]!=0){
+                count += move[down];
+                move[down] = 0;
+                nowIndex = down;
+                flag = true;
+                break;
+            }
+        }
+
+        return flag ? count : 0;
     }
 
     public static void main(String[] args) {
-        String name= "JEROENZ";
+        String name= "JEROEN";
+
         System.out.println(new JoyStick().solution(name));
+        System.out.println(move.length);
     }
 }
