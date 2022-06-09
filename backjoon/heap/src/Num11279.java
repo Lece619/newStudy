209f04018c4342cd7cb9f57efca93a549ddfa0c8
L <lece619@gmail.com>
2022-06-09 17:32:50 +0900

    ok

--- backjoon/heap/src/Num11279.java
+++ backjoon/heap/src/Num11279.java
@@ -1,12 +1,14 @@
-import java.util.PriorityQueue;
-import java.util.Scanner;
 
 //https://www.acmicpc.net/problem/11279
 //백준 - 최대 힙
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
 public class Num11279 {
 
     public static void main(String[] args) {
-        PriorityQueue<Integer> pQueue = new PriorityQueue<>();
+        PriorityQueue<Integer> pQueue = new PriorityQueue<>(Collections.reverseOrder());
         Scanner sc = new Scanner(System.in);
         StringBuilder s  = new StringBuilder();
         int n = sc.nextInt();
@@ -15,10 +17,14 @@ public class Num11279 {
             if(command==0){
                 if(pQueue.isEmpty()){
                     s.append("0\n");
-                }else{
-                    s.append(pQueue.poll()+"\n");
+                }else {
+                    s.append(pQueue.poll() + "\n");
                 }
             }
+            else{
+                pQueue.add(command);
+            }
         }
+        System.out.println(s.toString());
     }
 }
