f72694ba08bdef8ba2efca7ad0ac71b8bdb9d3b0
Lece <lece619@gmail.com>
2022-06-12 20:48:13 +0900

    Update Num1655.java

--- backjoon/heap/src/Num1655.java
+++ backjoon/heap/src/Num1655.java
@@ -11,5 +11,27 @@ public class Num1655 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int n  = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(((o1, o2) -> o2-o1));
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int i = 0; i <n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if(maxHeap.size() == minHeap.size()){
+                maxHeap.add(num);
+
+                if(!minHeap.isEmpty() && maxHeap.peek() > minHeap.peek()){
+                    maxHeap.add(minHeap.poll());
+                    minHeap.add(maxHeap.poll());
+                }
+            }else{
+                minHeap.add(num);
+                if(maxHeap.peek() > minHeap.peek()){
+                    maxHeap.add(minHeap.poll());
+                    minHeap.add(maxHeap.poll());
+                }
+            }
+            sb.append(maxHeap.peek()).append("\n");
+        }
+        System.out.println(sb.toString());
     }
 }
