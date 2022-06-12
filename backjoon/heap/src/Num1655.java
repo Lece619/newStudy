2c7a88780d0f0e0b11c1215d6ca53efe15cd07b1
Lece <lece619@gmail.com>
2022-06-12 19:49:25 +0900

    Update Num1655.java

--- backjoon/heap/src/Num1655.java
+++ backjoon/heap/src/Num1655.java
@@ -11,35 +11,5 @@ public class Num1655 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int n  = Integer.parseInt(br.readLine());
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        Stack<Integer> stack = new Stack<>();
-        String result = "";
-        for (int i = 0; i < n; i++) {
-            int nextNum = Integer.parseInt(br.readLine());
-
-            arrayList.add(nextNum);
-            stack.add(nextNum);
-        }
-        Collections.sort(arrayList);
-        int idx = n / 2;
-        if( n % 2 != 0){
-            result = arrayList.get(idx)+("\n")+result;
-            //sb.append(arrayList.get(idx)).append("\n");
-            arrayList.remove(stack.pop());
-        }
-        idx--;
-        while(!stack.isEmpty()){
-            result = arrayList.get(idx)+("\n")+result;
-
-//            sb.append(arrayList.get(idx)).append("\n");
-            arrayList.remove(stack.pop());
-            result = arrayList.get(idx)+("\n")+result;
-
-//            sb.append(arrayList.get(idx)).append("\n");
-            arrayList.remove(stack.pop());
-            idx--;
-        }
-        System.out.println(result);
-//        System.out.println(sb.toString());
     }
 }
