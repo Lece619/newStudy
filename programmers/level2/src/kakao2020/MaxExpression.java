d5fba5e8fe553c16da053759eb53507f907dcaae
L <lece619@gmail.com>
2022-06-14 00:39:29 +0900

    maxExpressino

--- programmers/level2/src/kakao2020/MaxExpression.java
+++ programmers/level2/src/kakao2020/MaxExpression.java
@@ -3,22 +3,70 @@
 
 package kakao2020;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class MaxExpression {
 
-    public long solution(String expression) {
-        String[] nums = expression.split("[-+*]");
-        String express = expression.replaceAll("\\d","");
-        long answer = 0;
+    static String[] priority;
+    static long max;
+    static LinkedList<String> list;
 
-        System.out.println(express);
-        for (String num : nums) {
-            System.out.print("  " + num);
+    public long solution(String expression) {
+        priority= new String[]{"+-*", "+*-", "-*+", "-+*", "*+-", "*-+"};
+        max = 0;
+        String[] nums = expression.split("[-+*]");
+        String[] express = expression.replaceAll("[0-9]","").split("");
+        long answer = 0;
+        list = new LinkedList<>();
+        list.add(nums[0]);
+        for (int i = 0; i < nums.length-1; i++) {
+            list.add(express[i]);
+            list.add(nums[i+1]);
         }
-        return answer;
+        for (String ex : priority) {
+            calNum(ex);
+        }
+        return max;
     }
+
+    public void calNum(String express){
+        LinkedList<String> tempList = (LinkedList<String>) list.clone();
+        for (int i = 0; i < express.length(); i++) {
+            int idx=0;
+            String command =express.charAt(i)+"";
+            while((idx=tempList.indexOf(command))!=-1){
+                //계산.
+                String str = calWith(tempList.get(idx-1),command,tempList.get(idx+1));
+                tempList.remove(idx-1);
+                tempList.remove(idx-1);
+                tempList.remove(idx-1);
+                tempList.add(idx-1,str);
+            }
+        }
+        max = Math.max(max,Math.abs(Long.parseLong(tempList.get(0))));
+    }
+
+    public String calWith(String n1,String command ,String n2){
+        long num1 = Long.parseLong(n1);
+        long num2 = Long.parseLong(n2);
+        switch (command) {
+            case "+":
+                return (num1+num2)+"";
+
+            case "*":
+                return (num1 *num2)+"";
+
+            case "-":
+                return (num1-num2)+"";
+        }
+        return null;
+    }
+
 
     public static void main(String[] args) {
         String expression = "100-200*300-500+20";
         new MaxExpression().solution(expression);
+        System.out.println(max);
     }
 }
