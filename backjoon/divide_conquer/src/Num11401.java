e2e252920904ea71085eaf85ddde2b90186e41f7
L <lece619@gmail.com>
2022-06-13 15:31:42 +0900

    Update Num11401.java

--- backjoon/divide_conquer/src/Num11401.java
+++ backjoon/divide_conquer/src/Num11401.java
@@ -4,16 +4,43 @@
 import java.util.Scanner;
 
 public class Num11401 {
+    static long p = 1000000007;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n,k;
         n = sc.nextInt();
         k = sc.nextInt();
-        System.out.println(combi(n,k));
+        //c(n k)  =  n! /  k!(n-k)!
+        long nFac = fac(n);
+        long knFac = fac(k) * fac(n-k) % p;
+
+        System.out.println( ( nFac * pow(knFac,p-2) ) % p );
+
     }
 
-    public static int combi(int n, int k){
-        if(n==k||k==0) return 1;
-        return (combi(n-1,k) + combi(n-1, k-1))% 1000000007;
+    public static long fac(long num){
+        long facNum = 1L;
+        while(num>1){
+            facNum = facNum * num % p;
+            num--;
+        }
+        return facNum;
     }
+
+    public static long pow(long num, long powNum){
+        long result = 1L;
+
+        while(powNum>0){
+
+            if(powNum % 2 == 1){
+                result = (result * num ) % p;
+            }
+
+            num = ( num * num ) % p;
+            powNum /= 2;
+        }
+
+        return result;  
+    }
+
 }
