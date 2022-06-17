8a31e56497ec3d060a0f70c163e5f651f24eaba5
L <lece619@gmail.com>
2022-06-17 14:59:06 +0900

    Update Num10830.java

--- backjoon/divide_conquer/src/Num10830.java
+++ backjoon/divide_conquer/src/Num10830.java
@@ -10,20 +10,35 @@ public class Num10830 {
 
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
-        int B = sc.nextInt();
+        long B = sc.nextLong();
 
-        int[][] matrix = new int[N][N];
+        long[][] matrix = new long[N][N];
+        long[][] result = new long[N][N];
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 matrix[i][j] = sc.nextInt();
             }
+            result[i][i] = 1;
+        }
+        while(B > 0){
+            if(B % 2 == 1){
+                result = multiMatrix( result, matrix );
+            }
+            B /= 2;
+            matrix = multiMatrix( matrix, matrix );
         }
 
+        for (long[] ints : result) {
+            for (long anInt : ints) {
+                System.out.print(anInt + " ");
+            }
+            System.out.println();
+        }
     }
 
-    public int[][] mutiMatrix(int[][] matrix1,int[][] matrix2){
-        int[][] resutlMatrix = new int[N][N];
+    public static long[][] multiMatrix(long[][] matrix1,long[][] matrix2){
+        long[][] resutlMatrix = new long[N][N];
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
