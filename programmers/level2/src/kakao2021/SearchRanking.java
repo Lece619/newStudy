fa22da16c74c7d632c3f56d92a4704b0003d4ecc
L <lece619@gmail.com>
2022-06-17 17:55:43 +0900

    Create SearchRanking.java

---
+++ programmers/level2/src/kakao2021/SearchRanking.java
@@ -0,0 +1,16 @@
+//https://programmers.co.kr/learn/courses/30/lessons/72412
+//        코딩테스트 연습
+//        2021 KAKAO BLIND RECRUITMENT
+//        순위 검색
+package kakao2021;
+
+public class SearchRanking {
+
+    class Solution {
+        public int[] solution(String[] info, String[] query) {
+            int[] answer = {};
+            return answer;
+        }
+    }
+
+}
