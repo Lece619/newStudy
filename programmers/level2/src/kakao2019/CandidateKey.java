a148fd3634fba87cdca26d176f66b9d50d1bb406
L <lece619@gmail.com>
2022-07-12 18:11:07 +0900

    Update CandidateKey.java

--- programmers/level2/src/kakao2019/CandidateKey.java
+++ programmers/level2/src/kakao2019/CandidateKey.java
@@ -5,6 +5,51 @@
 
 package kakao2019;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class CandidateKey {
     //후보키에대한 문제풀이
+    ArrayList<String> keySet;
+    StringBuilder makeKey2 = new StringBuilder();
+    int columnSize;
+    public int solution(String[][] relation) {
+        int answer = 0;
+        columnSize = relation[0].length;
+        StringBuilder makeKey = new StringBuilder();
+        keySet = new ArrayList<>();
+
+        makeKeySet(0);
+        System.out.println(keySet);
+        return answer;
+    }
+
+    public void makeKeySet(int a){
+        System.out.println(makeKey2.toString());
+        for (int i = a; i < columnSize; i++) {
+            if(makeKey2.indexOf(i+"")!=-1) {
+                continue;
+            }
+            makeKey2.append(i+" ");
+            keySet.add(makeKey2.toString());
+            makeKeySet(i);
+            makeKey2.delete(makeKey2.length()-2,makeKey2.length());
+        }
+    }
+    public static void main(String[] args) {
+        CandidateKey test = new CandidateKey();
+
+        String[][] relation = {{"100","ryan","music","2"},
+        {"200","apeach","math","2"},
+        {"300","tube","computer","3"},
+        {"400","con","computer","4"},
+        {"500","muzi","music","3"},
+        {"600","apeach","music","2"}};
+
+        StringBuilder st = new StringBuilder();
+        st.append("sdfs");
+        st.delete(st.length()-2,st.length());
+        System.out.println(st.toString());
+        test.solution(relation);
+    }
 }
