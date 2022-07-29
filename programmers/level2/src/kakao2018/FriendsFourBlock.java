bf10b08f908ac4f450f2f77e09a1ee1d7d42625c
L <lece619@gmail.com>
2022-07-29 12:05:11 +0900

    Update FriendsFourBlock.java

--- programmers/level2/src/kakao2018/FriendsFourBlock.java
+++ programmers/level2/src/kakao2018/FriendsFourBlock.java
@@ -6,11 +6,62 @@ https://school.programmers.co.kr/learn/courses/30/lessons/17679
 */
 package kakao2018;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class FriendsFourBlock {
+    ArrayList<Character>[] blocks;
+    boolean[][] check;
+    boolean flag;
 
     public int solution(int m, int n, String[] board) {
+        blocks = new ArrayList[board[0].length()];
+        check = new boolean[board[0].length()][board.length];
+        // 초기화
+        for (int i = 0; i < blocks.length; i++) {
+            blocks[i] = new ArrayList<>();
+        }
+        //역순으로 읽어들인다.
+        for (int i = board.length-1; i >= 0; i--) {
+            String str = board[i];
+            for (int j = 0; j < str.length(); j++) {
+                blocks[j].add(str.charAt(j));
+            }
+        }
+
+        flag = true;
+        while(flag){
+            for (int i = 0; i < blocks.length-1; i++) {
+                for (int j = 0; j < blocks[0].size()-1; j++) {
+                    if(blocks[i].get(j)=='0'){
+                        break;
+                    }
+                    checkBlock(i, j);
+                }
+            }
+        }
+
+/*        System.out.println("blocks = " + blocks[0]);
+        System.out.println("blocks = " + blocks[1]);
+        System.out.println("blocks = " + blocks[2]);
+        System.out.println("blocks = " + blocks[3]);
+        System.out.println("blocks = " + blocks[4]);*/
+
+
         int answer = 0;
         return answer;
     }
 
+    public void checkBlock(int x, int y){
+
+    }
+
+    public static void main(String[] args) {
+        FriendsFourBlock test = new FriendsFourBlock();
+        int m = 4;
+        int n = 5;
+        String[] board = {"CCBDE", "AAADE", "AAABF", "CCBBF"};
+        test.solution(m, n, board);
+
+    }
 }
