42b874144e052baad77ff7780ba2fa253fd9d7b8
L <lece619@gmail.com>
2022-07-29 10:51:03 +0900

    프렌즈 4블록

---
+++ programmers/level2/src/kakao2018/FriendsFourBlock.java
@@ -0,0 +1,16 @@
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/17679
+        코딩테스트 연습
+        2018 KAKAO BLIND RECRUITMENT
+        [1차] 프렌즈4블록
+*/
+package kakao2018;
+
+public class FriendsFourBlock {
+
+    public int solution(int m, int n, String[] board) {
+        int answer = 0;
+        return answer;
+    }
+
+}
