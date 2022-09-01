7a85d9b7142fdcdb4890b732aacbe8b81d40bfcf
L <lece619@gmail.com>
2022-09-01 16:23:11 +0900

    Create RepeatChangeBinary.java

---
+++ programmers/level2/src/challenge/RepeatChangeBinary.java
@@ -0,0 +1,23 @@
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/70129
+        코딩테스트 연습
+        월간 코드 챌린지 시즌1
+        이진 변환 반복하기
+*/
+
+
+package challenge;
+
+public class RepeatChangeBinary {
+
+    public int[] solution(String s) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        RepeatChangeBinary repeatChangeBinary = new RepeatChangeBinary();
+        String s = "";
+        repeatChangeBinary.solution(s);
+    }
+}
