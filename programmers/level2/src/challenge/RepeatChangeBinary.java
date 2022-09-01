bb71a32dd41ef88279df97831f2c4f6ed2d5bb1b
Lece <lece619@gmail.com>
2022-09-01 19:25:35 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/70129

--- programmers/level2/src/challenge/RepeatChangeBinary.java
+++ programmers/level2/src/challenge/RepeatChangeBinary.java
@@ -9,15 +9,28 @@ https://school.programmers.co.kr/learn/courses/30/lessons/70129
 package challenge;
 
 public class RepeatChangeBinary {
-
+    int[] answer = new int[2];
     public int[] solution(String s) {
-        int[] answer = {};
+        fineAnswer(s);
         return answer;
+    }
+
+    private void fineAnswer(String s) {
+        if(s.equals("1")){
+            return;
+        }else{
+            answer[0]++;
+            String tempS = s.replaceAll("0","");
+            answer[1] += s.length() - tempS.length();
+            s = Integer.toBinaryString(tempS.length());
+            fineAnswer(s);
+        }
     }
 
     public static void main(String[] args) {
         RepeatChangeBinary repeatChangeBinary = new RepeatChangeBinary();
-        String s = "";
-        repeatChangeBinary.solution(s);
+        String s = "1";
+        int[] solution = repeatChangeBinary.solution(s);
+        System.out.println("solution = " + solution[0] + " " + solution[1]);
     }
 }
