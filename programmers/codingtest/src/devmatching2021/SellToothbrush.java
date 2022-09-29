e8ba5af414cee23f1eb04457d31f6fb3415e3b83
Lece <lece619@gmail.com>
2022-09-29 16:02:34 +0900

    2021 Dev-Matching: 웹 백엔드 개발자(상반기)         다단계 칫솔 판매
    https://school.programmers.co.kr/learn/courses/30/lessons/77486

---
+++ programmers/codingtest/src/devmatching2021/SellToothbrush.java
@@ -0,0 +1,10 @@
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/77486
+2021 Dev-Matching: 웹 백엔드 개발자(상반기)
+        다단계 칫솔 판매
+*/
+
+package devmatching2021;
+
+public class SellToothbrush {
+}
