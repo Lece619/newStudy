e809d0947237e9118a3005033af6bfbc99dfa1ac
L <lece619@gmail.com>
2022-09-30 15:21:19 +0900

    Update BriansWorry4.java

--- programmers/level3/src/kakao2017/BriansWorry4.java
+++ programmers/level3/src/kakao2017/BriansWorry4.java
@@ -13,7 +13,7 @@ import java.util.HashSet;
 public class BriansWorry4 {
 
     ArrayList<Character> ruleWord = new ArrayList<>();
-    HashMap<String, ArrayList<Integer>> ruleIndex = new HashMap<>();
+    HashMap<Character, ArrayList<Integer>> ruleIndex = new HashMap<>();
 
     public String solution(String sentence) {
 
@@ -24,21 +24,28 @@ public class BriansWorry4 {
 
         findRuleWord(sentence);
 
-
-
         return "";
     }
 
     private void findRuleWord(String sentence) {
-        
+        for (int i = 0; i < sentence.length(); i++) {
+            char oneChar = sentence.charAt(i);
+            if(oneChar >= 'a'){
+                System.out.println("oneChar = " + oneChar);
+                if(!ruleIndex.containsKey(oneChar)){
+                    ruleWord.add(oneChar);
+                    ruleIndex.put(oneChar, new ArrayList<>());
+                }
+                ruleIndex.get(oneChar).add(i);
+            }
+        }
     }
 
 
     public static void main(String[] args) {
         String sentence = "HaEaLaLaObWORLDb";
 //        sentence = "SpIpGpOpNpGJqOqA";
-        sentence = "xAaAbAaAx";
-        sentence = "HELLOWORLD";
+//        sentence = "xAaAbAaAx";
         BriansWorry4 briansWorry = new BriansWorry4();
         System.out.println("briansWorry.solution(sentence) = " + briansWorry.solution(sentence));
     }
