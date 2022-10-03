4be040e0acc29cef1104d810235fe80e289eec7a
Lece <lece619@gmail.com>
2022-10-04 00:25:05 +0900

    level0

--- programmers/level0/src/basic/TestProblem2.java
+++ programmers/level0/src/basic/TestProblem2.java
@@ -14,8 +14,36 @@ public class TestProblem2 {
         }
         return sb.toString();
     }
-    public int solution(int price) {
+
+    public int solution(int n) {
+        int i;
+        for( i = 1 ; i <= 10; i++){
+            System.out.println("i = " + i);
+            System.out.println("facc(i) = " + facc(i));
+            System.out.println("facc = " + (facc(n) > n));
+            System.out.println("n = " + n);
+            if(facc(n) > n){
+                break;
+            }
+        }
+        return i;
+    }
+
+    public int facc(int n){
+        if(n==1 || n==0){
+            return 1;
+        }else{
+            return n * facc(n-1);
+        }
+    }
+    public int solution2(int price) {
         int answer = 0;
         return answer;
+    }
+
+    public static void main(String[] args) {
+        TestProblem2 testProblem2 = new TestProblem2();
+        int solution = testProblem2.solution(10);
+        System.out.println("solution = " + solution);
     }
 }
