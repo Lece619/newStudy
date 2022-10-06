b79e37f7940f09462079592714f9f71622ab2d29
L <lece619@gmail.com>
2022-10-06 14:53:48 +0900

    Update HikingCourse2.java

--- programmers/level3/src/kakao2022/HikingCourse2.java
+++ programmers/level3/src/kakao2022/HikingCourse2.java
@@ -7,21 +7,58 @@ https://school.programmers.co.kr/learn/courses/30/lessons/118669
 package kakao2022;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class HikingCourse2 {
 
-    int[][] dp;
     public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
         int[] answer = {};
-        dp = new int[n+1][n+1];
-        Arrays.sort(summits);
-        int intensity = Integer.MAX_VALUE;
-        int summitGate = 0;
+        PriorityQueue<Area> queue = new PriorityQueue<>();
+        queue.add(new Area(10, 4));
+        queue.add(new Area(10, 3));
+
+        Area peek = queue.peek();
+        System.out.println("peek.getIntensity() = " + peek.getNodeNum());
+
 
 
         return answer;
+    }
+
+    public class Area implements Comparable<Area>{
+        int intensity;
+        int nodeNum;
+
+        public Area(int intensity, int nodeNum) {
+            this.intensity = intensity;
+            this.nodeNum = nodeNum;
+        }
+
+        public int getIntensity() {
+            return intensity;
+        }
+
+        public void setIntensity(int intensity) {
+            this.intensity = intensity;
+        }
+
+        public int getNodeNum() {
+            return nodeNum;
+        }
+
+        public void setNodeNum(int nodeNum) {
+            this.nodeNum = nodeNum;
+        }
+
+        @Override
+        public int compareTo(Area o) {
+            if(this.getIntensity() == o.getIntensity()){
+                return this.getNodeNum() - o.getNodeNum();
+            }
+            return this.getIntensity() - o.getIntensity();
+        }
     }
 
 
