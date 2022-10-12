8ea4ab8a7d8cc9d39f913112895b50f1f9d3584a
Lece <lece619@gmail.com>
2022-10-12 14:38:46 +0900

    Create DartCount2.java

---
+++ programmers/level3/src/normal/DartCount2.java
@@ -0,0 +1,17 @@
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/131129
+        연습문제
+        카운트 다운
+*/
+package normal;
+
+public class DartCount2 {
+    public int[] solution(int target) {
+        int[] answer = new int[2];
+        return answer;
+    }
+    public static void main(String[] args) {
+        DartCount2 dartCount = new DartCount2();
+        System.out.println("dartCount.solution(21) = " + dartCount.solution(178));
+    }
+}
