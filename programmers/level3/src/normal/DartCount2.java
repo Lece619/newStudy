61abfc69726c3ccb2e5929ba36d1c05a1c85f0db
Lece <lece619@gmail.com>
2022-10-13 01:20:07 +0900

    연습문제         카운트 다운
            연습문제
            카운트 다운
    https://school.programmers.co.kr/learn/courses/30/lessons/131129

--- programmers/level3/src/normal/DartCount2.java
+++ programmers/level3/src/normal/DartCount2.java
@@ -1,14 +1,18 @@
 /*
 https://school.programmers.co.kr/learn/courses/30/lessons/131129
-        연습문제
-        카운트 다운
+            연습문제
+            카운트 다운
 */
 package normal;
+
+import java.util.Arrays;
 
 public class DartCount2 {
     public int[] solution(int target) {
         int[] answer = new int[2];
-        int[][] findTarget = new int[100_061][2];
+        int[][] findTarget = new int[2][100_061];
+        Arrays.fill(findTarget[0], Integer.MAX_VALUE);
+        findTarget[0][0] = 0;
         int[] step = new int[61];
         int[] singleBull = new int[61];
 
@@ -17,14 +21,24 @@ public class DartCount2 {
         for (int i = 0; i <= target; i++) {
             makeNextStep(findTarget, i, step, singleBull);
         }
+
+        answer[0] = findTarget[0][target];
+        answer[1] = findTarget[1][target];
         return answer;
     }
 
     private void makeNextStep(int[][] findTarget, int idx, int[] step, int[] singleBull) {
-        int nowStep = findTarget[idx][0];
-        int nowBull = findTarget[idx][1];
+        int nowStep = findTarget[0][idx];
+        int nowBull = findTarget[1][idx];
         for (int i = 1; i < 61; i++) {
-
+            int newStep = nowStep + step[i];
+            int newSingle = nowBull + singleBull[i];
+            if(findTarget[0][idx+i] > newStep){
+                findTarget[0][idx+i] = newStep;
+                findTarget[1][idx+i] = newSingle;
+            } else if ( findTarget[0][idx+i] == newStep) {
+                findTarget[1][idx + i] = Math.max(findTarget[1][idx+i], newSingle);
+            }
         }
     }
 
