b4b9763a06fc2e16e4d307716e2e935f3dd8cc34
Lece <lece619@gmail.com>
2022-11-06 15:05:24 +0900

    Create : Level3 2018 KAKAO BLIND RECRUITMENT         [1차] 추석 트래픽

---
+++ programmers/level3/src/kakao2018/TrafficOfThanksGiving.java
@@ -0,0 +1,23 @@
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/17676
+        2018 KAKAO BLIND RECRUITMENT
+        [1차] 추석 트래픽
+*/
+
+package kakao2018;
+
+public class TrafficOfThanksGiving {
+
+    public int solution(String[] lines) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String[] lines = {"2016-09-15 01:00:04.001 2.0s",
+                "2016-09-15 01:00:07.000 2s"};
+        TrafficOfThanksGiving trafficOfThanksGiving = new TrafficOfThanksGiving();
+        int solution = trafficOfThanksGiving.solution(lines);
+        System.out.println("solution = " + solution);
+    }
+}
