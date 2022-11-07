feb49f4c83e68be69cd5f4521909234bdf62c504
Lece <lece619@gmail.com>
2022-11-07 23:06:00 +0900

    Update StreamArr.java

--- programmers/level0/src/basic3/StreamArr.java
+++ programmers/level0/src/basic3/StreamArr.java
@@ -4,6 +4,7 @@ import java.util.stream.IntStream;
 
 public class StreamArr {
     public int[] solution(int n) {
+        
 
         return IntStream.rangeClosed(1, n).filter(i-> i % 2 == 1).toArray();
     }
