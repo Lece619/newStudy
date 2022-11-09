3b72447cc4348986739fdd3bf774e8c136b99e7d
Lece <lece619@gmail.com>
2022-11-09 15:27:24 +0900

    Create : EvenOdd class

---
+++ programmers/level0/src/basic3/EvenOdd.java
@@ -0,0 +1,15 @@
+package basic3;
+
+import java.util.Arrays;
+
+public class EvenOdd {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+
+        answer[0] = (int) Arrays.stream(num_list).filter(i-> i % 2 == 0).count();
+        answer[1] = num_list.length - answer[0];
+
+        return answer;
+    }
+
+}
