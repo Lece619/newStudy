a4207a34ce39fc4bf06b5bff5081ff48b0824c29
Lece <lece619@gmail.com>
2022-11-15 17:55:26 +0900

    Creake(Level3) : IslandLink  탐욕법(Greedy)   섬 연결하기

---
+++ programmers/level3/src/normal/IslandLink.java
@@ -0,0 +1,15 @@
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/42861
+        탐욕법(Greedy)
+        섬 연결하기
+*/
+package normal;
+
+public class IslandLink {
+
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+        return answer;
+    }
+    
+}
