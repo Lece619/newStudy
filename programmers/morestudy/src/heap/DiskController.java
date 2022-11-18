8e2c646b10ced37460201c46edd7a99ab1c53493
Lece <lece619@gmail.com>
2022-11-18 20:31:21 +0900

    Update DiskController.java

--- programmers/morestudy/src/heap/DiskController.java
+++ programmers/morestudy/src/heap/DiskController.java
@@ -14,23 +14,76 @@ public class DiskController {
 
     public int solution(int[][] jobs) {
         int answer = 0;
-        Arrays.sort(jobs, Comparator.comparingInt(a -> a[0]));
         boolean[] done = new boolean[jobs.length];
+        int count = jobs.length;
         int time = 0;
+        Arrays.sort(jobs, Comparator.comparingInt(a -> a[0]));
+
+
+        while(count-- > 0){
+
+            PriorityQueue<Job> nextJobTime = getJobs(jobs, done, time);
+            Job nextJob = nextJobTime.poll();
+
+            answer += nextJob.getTime(time);
+            time = time + nextJob.getTerm() + Math.max(0, nextJob.getStart() - time);
+            done[nextJob.getIdx()] = true;
+        }
+    
+        return answer / jobs.length;
+    }
+
+    private PriorityQueue<Job> getJobs(int[][] jobs, boolean[] done, int time) {
+        PriorityQueue<Job> nextJobTime = new PriorityQueue<>(new Comparator<Job>() {
+            @Override
+            public int compare(Job o1, Job o2) {
+                return o1.getTime(time) - o2.getTime(time);
+            }
+        });
 
         for (int i = 0; i < jobs.length; i++) {
             int[] job = jobs[i];
             if(time < job[0]){
-                break;
+                if(nextJobTime.isEmpty() && !done[i]){
+                    nextJobTime.add(new Job(i, job[0], job[1]));
+                    break;
+                }
+            }
+            if(!done[i]) {
+                nextJobTime.add(new Job(i, job[0], job[1]));
             }
         }
-
-        return answer;
+        return nextJobTime;
     }
 
     static class Job{
+        int idx;
+        int start;
+        int term;
 
+        public Job(int idx, int start, int term) {
+            this.idx = idx;
+            this.start = start;
+            this.term = term;
+        }
+
+        public int getTerm() {
+            return term;
+        }
+
+        public int getTime(int time){
+            return Math.max(term,time + term - start);
+        }
+
+        public int getIdx() {
+            return idx;
+        }
+
+        public int getStart() {
+            return start;
+        }
     }
+
 
     public static void main(String[] args) {
         int[][] jobs = {{0, 3}, {1, 9}, {2, 6}};
