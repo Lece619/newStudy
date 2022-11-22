65ad19b429307f1d706e8fc23e73e833149bc8f1
Lece <lece619@gmail.com>
2022-11-22 19:23:21 +0900

    Solve(Level2) : 탐욕법(Greedy)         단속카메라

---
+++ programmers/morestudy/src/greed/SpeedTrap.java
@@ -0,0 +1,41 @@
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/42884
+        탐욕법(Greedy)
+        단속카메라
+*/
+
+package greed;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class SpeedTrap {
+
+    public int solution(int[][] routes) {
+
+        int answer = 0;
+        Arrays.sort(routes, Comparator.comparingInt(a->a[0]));
+        Arrays.sort(routes, Comparator.comparingInt(a->a[1]));
+
+        for (int i = 0; i < routes.length; i++) {
+            answer++;
+            int firstEnd = routes[i][1];
+            for (int j = i+1; j < routes.length; j++) {
+                int nextStart = routes[j][0];
+                if(nextStart > firstEnd){
+                    break;
+                }
+                i++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        SpeedTrap speedTrap = new SpeedTrap();
+        int[][] routes ={{-20,-15}, {-14,-5}, {-18,-13}, {-5,-3}};
+        int solution = speedTrap.solution(routes);
+        System.out.println(solution);
+    }
+}
