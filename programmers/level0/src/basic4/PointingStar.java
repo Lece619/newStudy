18922ba95205cbf0368d3460d6d210bc68d34236
Lece <lece619@gmail.com>
2022-12-13 20:21:47 +0900

    Solve(Level0) : 별 찍기 ..

---
+++ programmers/level0/src/basic4/PointingStar.java
@@ -0,0 +1,12 @@
+package basic4;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class PointingStar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        IntStream.rangeClosed(1,n).forEach(i-> System.out.println("*".repeat(i)));
+    }
+}
