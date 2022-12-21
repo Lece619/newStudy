031fffde6cf476c4365796d14be564c7f30104b2
Lece <lece619@gmail.com>
2022-12-21 22:19:30 +0900

    Solve(Level0) : stream filter joing check

--- programmers/level0/src/basic4/MaxMultiple.java
+++ programmers/level0/src/basic4/MaxMultiple.java
@@ -1,10 +1,14 @@
 package basic4;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class MaxMultiple {
     public int solution(int[] numbers) {
         Arrays.sort(numbers);
         return numbers[numbers.length-1] * numbers[numbers.length-2];
+    }
+    public String solution(String my_string, String letter) {
+        return my_string.chars().filter(i->(char)i != letter.charAt(0)).mapToObj(i->String.valueOf((char)i)).collect(Collectors.joining());
     }
 }
