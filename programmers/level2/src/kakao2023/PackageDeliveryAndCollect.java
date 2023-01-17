3136e9795cc2626a48cc9c83d421685475c38498
L <25758530+Lece619@users.noreply.github.com>
2023-01-17 19:08:00 +0900

    Create(Level2):         2023 KAKAO BLIND RECRUITMENT         택배 배달과 수거하기
    https://school.programmers.co.kr/learn/courses/30/lessons/150369

---
+++ programmers/level2/src/kakao2023/PackageDeliveryAndCollect.java
@@ -0,0 +1,24 @@
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/150369
+        2023 KAKAO BLIND RECRUITMENT
+        택배 배달과 수거하기
+*/
+
+package kakao2023;
+
+public class PackageDeliveryAndCollect {
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+        long answer = -1;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        PackageDeliveryAndCollect packageDeliveryAndCollect = new PackageDeliveryAndCollect();
+        int cap = 4;
+        int n = 5;
+        int[] deliveries = {1, 0, 3, 1, 2};
+        int[] pickups = {0, 3, 0, 4, 0};
+
+        packageDeliveryAndCollect.solution(cap, n, deliveries, pickups);
+    }
+}
