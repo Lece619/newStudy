06191df97667c8e253630bb217688590cf2f69fd
L <25758530+Lece619@users.noreply.github.com>
2023-01-25 21:42:58 +0900

    Create(Level2):         연습문제         시소 짝꿍
    https://school.programmers.co.kr/learn/courses/30/lessons/152996
    
    알고리즘 다시생각해보기

---
+++ programmers/level2/src/normal2/SeesawPartner.java
@@ -0,0 +1,39 @@
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/152996
+        연습문제
+        시소 짝꿍
+*/
+
+
+package normal2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SeesawPartner {
+    public long solution(int[] weights) {
+        long answer = 0;
+        Set<Integer> weightAll = new HashSet<>();
+
+        Arrays.stream(weights).forEach(weightAll::add);
+        answer = weights.length - weightAll.size();
+
+        weightAll.clear();
+        Arrays.stream(weights).forEach(i->{
+            weightAll.add(i * 2);
+            weightAll.add(i * 3);
+            weightAll.add(i * 4);
+        });
+
+        answer = weights.length * 3 - (answer * 2 + weightAll.size());
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        SeesawPartner seesawPartner = new SeesawPartner();
+        int[] weights = {100,180,360,100,270};
+        long solution = seesawPartner.solution(weights);
+        System.out.println("solution = " + solution);
+    }
+}
