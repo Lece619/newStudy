1e59ee48a5c98d205f73bf62116c6705accd9d2e
Lece <lece619@gmail.com>
2023-01-28 23:29:36 +0900

    Feat(Level2): 연습문제 시소 짝꿍 재풀이

--- programmers/level2/src/normal2/SeesawPartner.java
+++ programmers/level2/src/normal2/SeesawPartner.java
@@ -14,6 +14,7 @@ import java.util.Set;
 public class SeesawPartner {
     public long solution(int[] weights) {
         long answer = 0;
+        /* 틀린로직 다시생각
         Set<Integer> weightAll = new HashSet<>();
 
         Arrays.stream(weights).forEach(weightAll::add);
@@ -25,7 +26,7 @@ public class SeesawPartner {
             weightAll.add(i * 3);
             weightAll.add(i * 4);
         });
-
+        */
         answer = weights.length * 3 - (answer * 2 + weightAll.size());
         return answer;
     }
