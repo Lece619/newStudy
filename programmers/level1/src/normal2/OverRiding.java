959c45635cf8ef5ab13435be4ad312d45a29bfd8
Lece <lece619@gmail.com>
2023-03-22 18:16:19 +0900

    Create(Level 1): 연습문제         덧칠하기
    https://school.programmers.co.kr/learn/courses/30/lessons/161989

---
+++ programmers/level1/src/normal2/OverRiding.java
@@ -0,0 +1,15 @@
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/161989
+        연습문제
+        덧칠하기
+*/
+
+package normal2;
+
+public class OverRiding {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        return answer;
+    }
+    
+}
