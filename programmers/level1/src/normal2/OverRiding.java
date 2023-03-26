8d5e23f6ee2b62d160bf56359a66009e5c7b6583
Lece <lece619@gmail.com>
2023-03-26 20:54:48 +0900

    SolveI(Level1): 연습문제 덧칠하기
    https://school.programmers.co.kr/learn/courses/30/lessons/161989

--- programmers/level1/src/normal2/OverRiding.java
+++ programmers/level1/src/normal2/OverRiding.java
@@ -7,9 +7,26 @@ https://school.programmers.co.kr/learn/courses/30/lessons/161989
 package normal2;
 
 public class OverRiding {
+
     public int solution(int n, int m, int[] section) {
         int answer = 0;
+        int nowPaint = 0;
+        for (int needPaint : section) {
+            if(needPaint > nowPaint){
+                answer++;
+                nowPaint = needPaint + m - 1;
+            }
+        }
         return answer;
     }
 
+    public static void main(String[] args) {
+        int n = 8;
+        int m = 4;
+        int[] section = {2, 3, 6};
+
+        OverRiding overRiding = new OverRiding();
+        int solution = overRiding.solution(n, m, section);
+        System.out.println("solution = " + solution);
+    }
 }
