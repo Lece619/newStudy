f2bb07d2514414af05dc45a0dae1fbf1773f866d
Lece <lece619@gmail.com>
2023-04-01 19:56:08 +0900

    Create(Level 1):         연습문제         추억 점수
    https://school.programmers.co.kr/learn/courses/30/lessons/176963

---
+++ programmers/level1/src/normal2/MemoryScore.java
@@ -0,0 +1,14 @@
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/176963
+        연습문제
+        추억 점수
+*/
+
+package normal2;
+
+public class MemoryScore {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = {};
+        return answer;
+    }
+}
