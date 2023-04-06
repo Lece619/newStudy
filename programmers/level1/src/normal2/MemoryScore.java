44de01b46351c3725373871c400b1b093a831ff1
Lece <lece619@gmail.com>
2023-04-06 22:12:44 +0900

    Solve(Level1): 연습문제 추억 점수
    @ -6,16 +6,28 @@ https://school.programmers.co.kr/learn/courses/30/lessons/176963

--- programmers/level1/src/normal2/MemoryScore.java
+++ programmers/level1/src/normal2/MemoryScore.java
@@ -6,16 +6,28 @@ https://school.programmers.co.kr/learn/courses/30/lessons/176963
 
 package normal2;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class MemoryScore {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        int[] answer = {};
-        return answer;
+        Map<String, Integer> scores = IntStream.range(0, name.length)
+                .boxed()
+                .collect(Collectors.toMap(i -> name[i], i -> yearning[i]));
+
+        return Arrays.stream(photo).
+                mapToInt(photoMembers -> Arrays.stream(photoMembers)
+                        .filter(scores::containsKey)
+                        .mapToInt(scores::get).sum()
+                ).toArray();
     }
 
     public static void main(String[] args) {
-        String[] name = {};
-        int[] yearning = {};
-        String[][] photo = {{}};
+        String[] name = {"may", "kein", "kain", "radi"};
+        int[] yearning = {5, 10, 1, 3};
+        String[][] photo = {{"may", "kein", "kain", "radi"},{"brin", "brin", "brin", "deny"},{"kon", "kain", "may", "coni"}};
 
         MemoryScore memoryScore = new MemoryScore();
         int[] solution = memoryScore.solution(name, yearning, photo);
