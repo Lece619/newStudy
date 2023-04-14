e39932afb908f35b4e7051d286d2a592cb3bd14f
Lece <lece619@gmail.com>
2023-04-14 22:38:48 +0900

    Create(Level2):         연습문제         마법의 엘리베이터
    https://school.programmers.co.kr/learn/courses/30/lessons/148653
    
    DP를 사용해서 풀이하는법 생각

---
+++ programmers/level2/src/normal2/MagicElevator.java
@@ -0,0 +1,19 @@
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/148653
+        연습문제
+        마법의 엘리베이터
+*/
+package normal2;
+
+
+public class MagicElevator {
+    public int solution(int storey) {
+        String floor = String.valueOf(storey);
+        return 0;
+    }
+    public static void main(String[] args) {
+        MagicElevator magicElevator = new MagicElevator();
+        int solution = magicElevator.solution(16);
+        System.out.println("solution = " + solution);
+    }
+}
