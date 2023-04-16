fae26ef265b807721c91924d5009cdf79d1df198
Lece <lece619@gmail.com>
2023-04-16 23:43:03 +0900

    Revert "Revert "Solve(Level 1):         연습문제         달리기 경주""
    This reverts commit a2207e541674b0b0d8dcb3efda822995b7779144.

--- programmers/level1/src/normal2/RunningRace.java
+++ programmers/level1/src/normal2/RunningRace.java
@@ -6,18 +6,41 @@ https://school.programmers.co.kr/learn/courses/30/lessons/178871
 
 package normal2;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class RunningRace {
-    public String[] solution(String[] players, String[] callings) {
-        String[] answer = {};
-        return answer;
+    public String[] solution2(String[] players, String[] callings) {
+        List<String> playerRank = Arrays.stream(players).collect(Collectors.toList());
+        for (String calling : callings) {
+            int index = playerRank.indexOf(calling);
+            playerRank.remove(index--);
+            playerRank.add(index, calling);
+        }
+        return playerRank.toArray(new String[0]);
     }
+    public String[] solution(String[] players, String[] callings) {
+        Map<String, Integer> rank = IntStream.range(0, players.length)
+                .boxed()
+                .collect(Collectors.toMap(i -> players[i], i -> i));
 
+        for (String calling : callings) {
+            Integer calledPersonNum = rank.get(calling);
+            int downPersonNum = calledPersonNum - 1;
+            String downPerson = players[downPersonNum];
+            players[downPersonNum] = calling;
+            players[calledPersonNum] = downPerson;
+            rank.put(calling, downPersonNum);
+            rank.put(downPerson, calledPersonNum);
+        }
+
+        return players;
+    }
     public static void main(String[] args) {
         RunningRace runningRace = new RunningRace();
         String[] solution = runningRace.solution(
-                new String[]{"mumu", "soe", "poe", "kai"},
+                new String[]{"mumu", "soe", "poe", "kai", "mine"},
                 new String[]{"kai", "kai", "mine", "mine"}
         );
         Arrays.stream(solution).forEach(System.out::println);
