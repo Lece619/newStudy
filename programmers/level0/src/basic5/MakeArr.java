ed1d8da144f93647d76ce7706356ffab51e0ce76
Lece <lece619@gmail.com>
2023-05-01 22:59:06 +0900

    Solve(Level 0): 코딩 기초 트레이닝 배열의 길이를 2의 거듭제곱으로 만들기

---
+++ programmers/level0/src/basic5/MakeArr.java
@@ -0,0 +1,17 @@
+package basic5;
+
+public class MakeArr {
+
+    public int[] solution(int[] arr) {
+        int length = arr.length;
+        int idx = 0;
+        while(Math.pow(2,idx) < length){
+            idx++;
+        }
+        int[] answer = new int[(int) Math.pow(2,idx)];
+        for (int i = 0; i < arr.length; i++) {
+            answer[i] = arr[i];
+        }
+        return answer;
+    }
+}
