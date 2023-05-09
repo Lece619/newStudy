dfcb1283f49180a9f3b46902a86366d720b7638c
Lece <lece619@gmail.com>
2023-05-09 21:37:26 +0900

    Solve(Level 0): 코딩 기초 트레이닝 조건에 맞게 수열 변환하기 1

--- programmers/level0/src/basic5/MakeArr1.java
+++ programmers/level0/src/basic5/MakeArr1.java
@@ -1,10 +1,24 @@
 package basic5;
 
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class MakeArr1 {
 
     public int[] solution(int n, int k) {
-        return  IntStream.rangeClosed(1,n/k).map(i->i*k).toArray();
+        return IntStream.rangeClosed(1, n / k).map(i -> i * k).toArray();
     }
+
+    public int[] solution(int[] arr) {
+
+        return Arrays.stream(arr).map(i -> {
+                    if (i >= 50 && i % 2 == 0)
+                        return i / 2;
+                    if (i < 50 && i % 2 == 1)
+                        return i * 2;
+                    else
+                        return i;
+                }).toArray();
+    }
+
 }
