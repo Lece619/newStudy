aa789bffa30e7cc4c328a8be49ffd5b872a372f0
Lece <lece619@gmail.com>
2023-05-23 22:32:33 +0900

    Solve(Level 0): 코딩 기초 트레이닝 배열 조각하기

--- programmers/level0/src/basic6/CopyArrN.java
+++ programmers/level0/src/basic6/CopyArrN.java
@@ -7,4 +7,20 @@ public class CopyArrN {
         //COPY ARR UNTIL N
         return Arrays.stream(num_list).limit(n).toArray();
     }
+
+    public int[] solution(int[] arr, int[] query) {
+
+        for (int i = 0; i < query.length; i++) {
+            if (i % 2 == 0) {
+                arr = Arrays.stream(arr).limit(query[i] + 1).toArray();
+            } else {
+                arr = Arrays.stream(arr).skip(query[i]).toArray();
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        new CopyArrN().solution(new int[]{0, 1, 2, 3, 4, 5}, new int[]{4, 1, 2});
+    }
 }
