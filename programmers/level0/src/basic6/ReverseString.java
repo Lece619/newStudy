258f2eea585696e28c6db1dda7b0c3c80f4b7fe1
Lece <lece619@gmail.com>
2023-06-05 21:42:24 +0900

    Solve(Level 0): 코딩 기초 트레이닝 문자열 여러 번 뒤집기

--- programmers/level0/src/basic6/ReverseString.java
+++ programmers/level0/src/basic6/ReverseString.java
@@ -5,4 +5,11 @@ public class ReverseString {
         StringBuilder reverse = new StringBuilder(my_string.substring(s, e + 1)).reverse();
         return my_string.substring(0, s) + reverse + my_string.substring(e + 1);
     }
+    public String solution(String my_string, int[][] queries) {
+        String answer = my_string;
+        for (int[] query : queries) {
+            answer = solution(answer, query[0], query[1]);
+        }
+        return answer;
+    }
 }
