899e982dda37ea1190d9f66f918de9bf0bb6dc95
Lece <lece619@gmail.com>
2023-06-08 20:50:09 +0900

    Solve(Level 0): 코딩 기초 트레이닝 이어 붙인 수

---
+++ programmers/level0/src/basic6/OddEvenSum.java
@@ -0,0 +1,17 @@
+package basic6;
+
+
+public class OddEvenSum {
+    public int solution(int[] num_list) {
+        StringBuilder odd = new StringBuilder();
+        StringBuilder even = new StringBuilder();
+        for (int i : num_list) {
+            if (i % 2 == 0) {
+                even.append(i);
+            } else {
+                odd.append(i);
+            }
+        }
+        return Integer.parseInt(odd.toString()) + Integer.parseInt(even.toString());
+    }
+}
