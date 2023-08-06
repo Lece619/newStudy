517f2f9bb2d117f0b45afec72faadee2c5793f90
Lece <lece619@gmail.com>
2023-08-06 22:02:44 +0900

    Study: BufferStreamStudy

---
+++ programmers/morestudy/src/bufferStream/BufferStreamStudy.java
@@ -0,0 +1,44 @@
+package bufferStream;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BufferStreamStudy {
+    private static final byte[] DLT_SIGNATURE = {'D', 'L', 'T', ' '};
+
+    public List<DLTLog> readDLTLogs(String filename) throws IOException {
+        List<DLTLog> logs = new ArrayList<>();
+        try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(filename))) {
+            int signatureIndex = 0;
+            int b;
+            while ((b = bis.read()) != -1) {
+                if (b == DLT_SIGNATURE[signatureIndex]) {
+                    signatureIndex++;
+                    if (signatureIndex == DLT_SIGNATURE.length) {
+                        DLTLog log = readLog(bis);
+                        if (log != null) {
+                            logs.add(log);
+                        }
+                        signatureIndex = 0; // Reset the signature index to start searching again
+                    }
+                } else {
+                    signatureIndex = 0; // Reset the signature index if the byte doesn't match
+                }
+            }
+        }
+        return logs;
+    }
+
+    private DLTLog readLog(BufferedInputStream bis) throws IOException {
+        // Read the log data after the DLT signature, create and return a DLTLog object
+        
+
+        return null;
+    }
+}
+class DLTLog {
+    // DLT log data
+}
